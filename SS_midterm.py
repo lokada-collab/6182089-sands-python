bb31fc577ae3c0236ff694a1b2eac910aea87d5e
Lisa <l.okada@student.tudelft.nl>
2025-09-29 13:19:20 +0200

    Second Update

--- SS_midterm.py
+++ SS_midterm.py
@@ -5,3 +5,18 @@ import numpy as np, matplotlib.pyplot as plt
 # Time axis
 fs = 2000
 t = np.linspace(0, 1, fs, endpoint=False)  # 1 second, 2000 samples
+
+# Signals (5 Hz)
+f = 5.0
+
+# Sinusoid:
+
+sine   = np.sin(2*np.pi*f*t)
+
+plt.figure(); plt.plot(t, sine);   #new blank canvas
+plt.title("Figure 1 - Sinusoid (5 Hz)");   
+plt.xlabel("t [s]"); 
+plt.ylabel("Amplitude"); 
+plt.grid(True);     #Adding line grid onto the graph so that its easier to read
+
+plt.show()
