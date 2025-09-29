f59e74b2aec607abe04d6d47ff7890a667be5933
Lisa <l.okada@student.tudelft.nl>
2025-09-29 17:35:36 +0200

    Run sine wave

---
+++ Run.py
@@ -0,0 +1,17 @@
+from signals import generate_sine_wave
+import matplotlib.pyplot as plt 
+
+frequency = 5
+duration = 10
+sample_rate = 100
+
+wave = generate_sine_wave(frequency, duration, sample_rate)
+t = [i/sample_rate
+        for i in range(int(duration*sample_rate))]
+
+plt.plot(t,wave)
+plt.title("Sine Wave")
+plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
