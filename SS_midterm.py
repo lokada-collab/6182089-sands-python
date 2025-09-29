189466aec064dc458ec2142661d1a12cbe6a13a2
Lisa <l.okada@student.tudelft.nl>
2025-09-29 12:23:14 +0200

    First Update

---
+++ SS_midterm.py
@@ -0,0 +1,7 @@
+# Signals and Systems Python project - Lisa Okada (6182089)
+
+import numpy as np, matplotlib.pyplot as plt
+
+# Time axis
+fs = 2000
+t = np.linspace(0, 1, fs, endpoint=False)  # 1 second, 2000 samples
