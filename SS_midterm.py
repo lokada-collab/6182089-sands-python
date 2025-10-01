2d2ef30fa8cd9f82b1ada39f2b842f539c2bc834
Lisa <l.okada@student.tudelft.nl>
2025-10-01 12:06:35 +0200

    Third Update

--- SS_midterm.py
+++ SS_midterm.py
@@ -1,5 +1,19 @@
 # Signals and Systems Python project - Lisa Okada (6182089)
 
+# Fuctions:
+def sine_wave(fs):
+    np.linspace(0, 1, fs, endpoint=False)
+
+t = sine_wave(fs=2000)
+
+fs = 2000
+t = np.linspace(0, 1, fs, endpoint=False)  # 1 second, 2000 samples
+
+f = 5.0
+
+
+
+
 import numpy as np, matplotlib.pyplot as plt
 
 # Time axis
