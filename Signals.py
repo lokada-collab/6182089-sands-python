433cc90d96ac4fb4c4849efd25d151e9480bff20
Lisa <l.okada@student.tudelft.nl>
2025-10-02 11:13:46 +0200

    Adding forgotten codes to Sine Wave

--- Signals.py
+++ Signals.py
@@ -3,6 +3,7 @@ import numpy as np
 def generate_sine_wave(frequency, duration, sample_rate, amplitude):
     t = np.linspace(0, duration, sample_rate * duration, endpoint=False)
     y = amplitude * np.sin(2 * np.pi * frequency * t)
+    return t,y
 
     # t: time axis
     # def: defines a function
