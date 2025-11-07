7d4c1196463050624126a4f4f88054efad437c15
Lisa <l.okada@student.tudelft.nl>
2025-11-07 20:02:09 +0100

    Correcting sine wave test

--- test_signals.py
+++ test_signals.py
@@ -2,12 +2,12 @@ import numpy as np
 from signals import generate_sine_wave, generate_square_wave, time_shift, time_scale
 
 # Normal test for sine wave
-t, y = generate_sine_wave(1, 2, 1)
-assert len(t) == 1000 
-assert len(y) == 1000 
+t, y = generate_sine_wave(1, 2, 1000, 1) 
+assert len(t) == 2000
+assert len(y) == 2000
 
 # Edge test for sine wave
-t, y = generate_sine_wave(5, 0, 1)
+t, y = generate_sine_wave(5, 0, 1000, 0)
 assert np.allclose(y, 0)
 
 # Normal test for square wave
@@ -31,7 +31,7 @@ assert np.array_equal(t_shift, t)
 assert np.array_equal(y_shift, y)
 
 # Normal test for time scale
-t_scaled, y_scaled = time_scale(t, y, 0.5)  # compress
+t_scaled, y_scaled = time_scale(t, y, 0.5)
 assert len(t_scaled) == len(y_scaled)
 assert np.allclose(t_scaled, t * 0.5)
 
