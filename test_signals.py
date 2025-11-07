858dbbffc3051580f72bfd7b2bb331e83c05edd0
Lisa <l.okada@student.tudelft.nl>
2025-11-07 18:10:33 +0100

    Testing amiplitude

--- test_signals.py
+++ test_signals.py
@@ -4,3 +4,6 @@ from matplotlib.pyplot as plt
 t, y = generate_sine_wave(1, 2, 1)
 assert len(t) == 1000 
 assert len(y) == 1000 
+
+t, y = generate_sine_wave(5, 3, 1)
+assert np.isclose(max(y), 3, atol=1e-6)
